fa2b22963dc70cfc7d6774b41b273555128afafc
Brismar23 <72509730+Brismar23@users.noreply.github.com>
2023-12-07 21:40:56 -0600

    Creación de Javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"registroalumnos","l":"Alumno"},{"p":"registroalumnos","l":"Asignatura"},{"p":"registroalumnos","l":"GenNombres"},{"p":"registroalumnos","l":"GenNumInscripcion"},{"p":"registroalumnos","l":"RegistroAcadem"},{"p":"registroalumnos","l":"REGISTROALUMNOS"}];updateSearchResults();
