dcb23d3e46a47e30c9eea540fedb5e9b2df4679f
Sara-CarLa <111708879+Sara-CarLa@users.noreply.github.com>
2023-12-07 22:11:42 -0600

    Update GenNumInscripcion.java

--- src/registroalumnos/GenNumInscripcion.java
+++ src/registroalumnos/GenNumInscripcion.java
@@ -3,15 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package registroalumnos;
+
+/**
+ *
+ * @author equipo I
+ */
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-/**
- *
- * @author eduar
- */
 public class GenNumInscripcion {
+    /**
+     * Metodo que genera el numero de inscripcion de los alumnos
+     * @param promedio Toma como parametro el promedio de cada alumno para poder generar el numero de inscripcion
+     * @return regresa el numero de inscripcion del alumno
+     */
     public String generarIndicador(Double promedio){
 
         Double escolaridad;
@@ -31,8 +38,8 @@ public class GenNumInscripcion {
     }
 
     /**
-     * Se aplica la primera vez que ordena los numero de inscripcion
-     * @param alumnos Lista de alumnos a ordenar
+     * Metdo que ordena a los alumnos dependiendo de su numero de inscripcion
+     * @param alumnos recibe como parametro el numero de inscripcion del array de alumnos
      */
     public static void ordenarPorNumInscripcion(List<Alumno> alumnos){
         Comparator<Alumno> comparadorPorIndicador = Comparator.comparing(Alumno::getNumeroInscripcion).reversed();
@@ -40,20 +47,15 @@ public class GenNumInscripcion {
     }
     
     /**
-     * Se aplica cuando ya han sido ordenados los numeros de inscripcion al menos una vez
-     * @param alumnos Lista de alumnos a ordenar
+     * Segundo metodo que ordena a los alumnos agregados por su numero de inscripcion
+     * @param alumnos recibe como parametro un alumno de la lista de alumnos
      */
     public static void ordenarPorNumInscripcion2(List<Alumno> alumnos){
         Comparator<Alumno> comparadorPorIndicador = Comparator.comparing(Alumno::getNumeroInscripcion);
         Collections.sort(alumnos, comparadorPorIndicador);
     }
     
-    public static void ordenarPorPromedio(List<Alumno> alumnos){
-        Comparator<Alumno> comparadorPorPromedio = Comparator.comparing(Alumno::getPromedio);
-        Collections.sort(alumnos, comparadorPorPromedio);
-    }
 
     
     
 }
-
