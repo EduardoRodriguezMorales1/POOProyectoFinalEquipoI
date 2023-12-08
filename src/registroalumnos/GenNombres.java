6b8e14762ce18899d672d3bd5b73c789d3235ff8
Sara-CarLa <111708879+Sara-CarLa@users.noreply.github.com>
2023-12-07 22:10:08 -0600

    Update GenNombres.java

--- src/registroalumnos/GenNombres.java
+++ src/registroalumnos/GenNombres.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package registroalumnos;
-
+/**
+*
+* @author equipo I
+ */
 
 import java.io.RandomAccessFile; // 
 import java.util.Random; // Numeros aleatorios
